ab2b87d9cac36e8109dda03007cb307ea55f7d81
raju <raju@rajus-MacBook-Pro.local>
2017-08-06 15:56:40 +0530

    Added the Buttons

--- MathGenie/MathGenie/ViewController.swift
+++ MathGenie/MathGenie/ViewController.swift
@@ -10,13 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
             self.title = "MATHS !!!!!"
+            addRightNavItemOnView()
+
+    }
+    
+    func startGame() {
+        
+        
+        
+        
         
     }
+    
+    
+    func addRightNavItemOnView()
+    {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.setImage(UIImage(named: "Start.png"), for: .normal)
+        button.addTarget(self, action: #selector(ViewController.startGame), for: UIControlEvents.touchUpInside)
+        let rightBarButtonStart = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = rightBarButtonStart
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
